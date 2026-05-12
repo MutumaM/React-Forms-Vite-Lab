bacb0fdb39e99f2953b36a27a1a3e8ff1be47c9b
MutumaM <jaydenmwongera@gmail.com>
2026-05-13 00:17:14 +0300

    filterfeature

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -5,21 +5,21 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText] = useState('')
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
+    if (selectedCategory === "All" && item.category !== selectedCategory) return false;
+      return item.name.toLowerCase().includes(searchText.toLowerCase())
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} setSearchText={searchText}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
